15915cd26f1587a27245b597917d858945e925c9
Rohan Singh <riskysingh897@gmail.com>
2025-02-17 12:51:09 +0530

    Vas-United Project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
