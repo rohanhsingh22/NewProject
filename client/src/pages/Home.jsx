7a4e11121893f14f9b0b3b060b52cc15d7793a29
khushbu-sahu <khushboosahu15@outlook.com>
2025-02-18 15:54:29 +0530

    Listing and testimonials section completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,15 +3,19 @@ import HeroSection from '../components/HeroSection'
 // import HotProperty from '../components/HotProperty'
 import LatestProperties from '../components/LatestProperty'
 import Services from '../components/Services'
+import Listing from '../components/Listing'
+import Testimonial from '../components/Testimonial'
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       {/* <HotProperty/> */}
+      <Listing/>
       <Services/>
       <LatestProperties/>
       <RealEstateAgents/>
+      <Testimonial/>
 
     </div>
   )
