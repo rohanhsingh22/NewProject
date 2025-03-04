7b22edb0d0f7fa1f7ce7835564504402250f4918
khushbu-sahu <khushboosahu15@outlook.com>
2025-03-04 14:34:47 +0530

    list of property done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SignIn from './components/SignIn'
+import ListOfPropert from './components/ListOfPropert'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/listing" element={<ListOfPropert />} />
       </Routes>
     <Footer/>
     </BrowserRouter>
