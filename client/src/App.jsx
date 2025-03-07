885414e9ac33e82859e75892012423b8cfde2206
khushbu-sahu <khushboosahu15@outlook.com>
2025-03-07 14:20:14 +0530

    ListOfProperty

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import SignIn from './components/SignIn'
 import PropertyDetail from './pages/PropertyDetail'
+import ListOfPropert from './components/ListOfPropert'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/listing" element={<ListOfPropert/>}/>
         <Route path="/property/:id" element={<PropertyDetail />} />
       </Routes>
       <Footer />
